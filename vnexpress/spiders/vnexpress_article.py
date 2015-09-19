31c42fb9dd7a3659ca0e22bbe7da08c46f3dc519
nguyen viet bang <vbang.nguyen@gmail.com>
2015-09-20 01:16:39 +0700

    feed terms vietnam

--- vnexpress/spiders/vnexpress_article.py
+++ vnexpress/spiders/vnexpress_article.py
@@ -1,30 +1,58 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time
+import re
+
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor  as lxml
-import scrapy
-import pdb
 import html2text
-from elasticsearch import Elasticsearch
-import re
+import redis
 from  scrapy.exceptions import CloseSpider
 
 
-class Article(scrapy.Item):
-    title = scrapy.Field()
-    description = scrapy.Field()
-    url = scrapy.Field()
+class RedisQueue(object):
+    """Simple Queue with Redis Backend"""
+
+    def __init__(self, name, namespace='queue', **redis_kwargs):
+        self.__db = redis.Redis(**redis_kwargs)
+        self.key = '%s:%s' % (namespace, name)
+
+    def qsize(self):
+        """Return the approximate size of the queue."""
+        return self.__db.llen(self.key)
+
+    def empty(self):
+        """Return True if the queue is empty, False otherwise."""
+        return self.qsize() == 0
+
+    def put(self, item):
+        """Put item into the queue."""
+        self.__db.rpush(self.key, item)
+
+    def get(self, block=True, timeout=None):
+        """Remove and return an item from the queue.
+
+        If optional args block is true and timeout is None (the default), block
+        if necessary until an item is available."""
+        if block:
+            item = self.__db.blpop(self.key, timeout=timeout)
+        else:
+            item = self.__db.lpop(self.key)
+
+        if item:
+            item = item[1]
+        return item
+
+    def get_nowait(self):
+        """Equivalent to get(False)."""
+        return self.get(False)
 
 
-es_client = Elasticsearch()
-index_name = "vietnam"
-doctype = "article"
+def clean_string(value):
+    return ' '.join(value.split()).strip().replace('\x00', '')
 
-# es_client.index(index=index_name, doc_type=doctype, id=self.text, body=self.to_dict())
 
-def write_to_file(filename, result):
-    output = open(filename, 'wb')
+def write_word_list(filename, result):
+    output = open(filename, 'a')
     output.writelines(["%s\n" % item.encode('utf-8') for item in result])
     output.close()
 
@@ -44,14 +72,25 @@ class VnexpressArticle(CrawlSpider):
              callback='create_article'),
     ]
     is_write = False
+    queue = RedisQueue("vietnam", host="107.155.114.223")
+    word_to_write_list = []
+
+    def write_to_file(self, word):
+        self.word_to_write_list.append(word)
+        if len(self.word_to_write_list) % 1000 == 0:
+            write_word_list("vietnam.txt", self.word_to_write_list)
+            self.word_to_write_list = []
+
 
     def add_text(self, text):
         data = re.sub("”|\"|\n|`|-|=|[|]|'|;|/|\.|,|~|!|@|#|$|%|^|&|\*|:|\(|\)|_|\+|}|{|>|<|\?", " ", text)
         for sub in data.split(" "):
-            #text_clean= sub.replace("â€","")
+            # text_clean= sub.replace("â€","")
             text_clean = sub.strip().lower()
-            if len(text_clean) >1 and text_clean.isdigit() == False and text_clean not in self.words:
+            if len(text_clean) > 1 and text_clean.isdigit() == False and text_clean not in self.words:
                 self.words.append(text_clean)
+                self.queue.put({"lang": "vietnam", "keyword": text_clean})
+                self.write_to_file(text_clean)
 
 
     def add_big_text(self, results):
@@ -66,16 +105,8 @@ class VnexpressArticle(CrawlSpider):
                 0].extract()
             results = response.xpath(
                 "//div[@id='detail_page']//div[contains(@class,'main_content_detail')]//div[@id='left_calculator']//div[@class='fck_detail width_common']//p//text()")
-            if self.is_write == False and len(self.words) > 200000:
-                print "Write to file "
-                self.is_write = True
-                unique_terms= set(self.words)
-                print "lengh1 %s length2 %s"%(len(self.words),len(unique_terms))
-                write_to_file("vietnam.txt", unique_terms)
-                raise CloseSpider('bandwidth_exceeded')
-            else:
-                print "lengh1 %s " %(len(self.words))
-                self.add_big_text(results)
+            self.add_big_text(results)
+            print "Length %s " % (len(self.words))
         except:
             return None
 
